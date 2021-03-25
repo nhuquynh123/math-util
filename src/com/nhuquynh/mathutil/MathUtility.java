2daabde681f321cb84a7a87db4e5ad64e4adbdfd
nhuquynh123 <quynhdtnse140541@fpt.edu.vn>
2021-03-25 16:28:38 +0700

    RECURSION UPDATED & GREEN 4EVER AGAIN

--- src/com/nhuquynh/mathutil/MathUtility.java
+++ src/com/nhuquynh/mathutil/MathUtility.java
@@ -24,12 +24,19 @@ public class MathUtility {
         throw new IllegalArgumentException("n must be between 0..20");
         
         //song sot duoc den day thi n=0,1,..20
+        //sua code, sua ham giai thua cho gon, dung de quy
         if (n==0 || n==1) 
-            return 1; //0!, 1! = 
-        long result = 1; // xanh local va server // gia no = 
-        for(int i = 2; i<=n; i++)
-            result *=i;
+            return 1; //0! = 1! = 1 tinh luon cho nhanh 
+        return n * getFactorial(n-1);
+        //n! = n* (n-1)!
+        //de quy, vong lai chinh minh voi mot quy mo khac 
+        //recursion, lam kheo neu khong no di mai mai 
+        //stackoverflow.com
         
-        return result; //nhan tu 2 3 4 5 6 7 ....
+//        long result = 1; // xanh local va server // gia no = 
+//        for(int i = 2; i<=n; i++)
+//            result *=i;
+//        
+//        return result; //nhan tu 2 3 4 5 6 7 ....
     }
 }

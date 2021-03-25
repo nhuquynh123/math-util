95539f6f56b59d8b1f618a6df9c505078e78440d
nhuquynh123 <quynhdtnse140541@fpt.edu.vn>
2021-03-25 15:44:29 +0700

    Update core code & Green 4EVER

--- src/com/nhuquynh/mathutil/MathUtility.java
+++ src/com/nhuquynh/mathutil/MathUtility.java
@@ -26,7 +26,7 @@ public class MathUtility {
         //song sot duoc den day thi n=0,1,..20
         if (n==0 || n==1) 
             return 1; //0!, 1! = 
-        long result = 0;
+        long result = 1; // xanh local va server // gia no = 
         for(int i = 2; i<=n; i++)
             result *=i;
         
